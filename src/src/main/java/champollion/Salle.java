9bf7de78552eabd8d1261e0eb0738da6463933d1
melanie6081 <116157079+melanie6081@users.noreply.github.com>
2022-12-13 13:31:59 +0100

    Add files via upload

---
+++ src/src/main/java/champollion/Salle.java
@@ -0,0 +1,11 @@
+package champollion;
+
+public class Salle {
+    private final String intitule;
+    private final int capacite;
+
+    public Salle(String intitule, int capacite) {
+        this.intitule = intitule;
+        this.capacite = capacite;
+    }
+}
